faf55da2f848a6ba2073b81a74b004c144aaa4cc
awanaw25 <putrakembang47@gmail.com>
2024-03-25 03:55:56 +0700

    Materi 1

---
+++ myoop/bin/myoop.dart
@@ -0,0 +1,5 @@
+import 'package:myoop/myoop.dart' as myoop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${myoop.calculate()}!');
+}
---
+++ myoop/test/myoop_test.dart
@@ -0,0 +1,8 @@
+import 'package:myoop/myoop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
